23215e3e61bd8dc21e52f8f407bc080b4d650f01
PiggehCakes Jordan <47580405+PiggehCakes@users.noreply.github.com>
2021-01-28 17:29:21 -0500

    working grab

--- Assets/Scripts/ItemGrab.cs
+++ Assets/Scripts/ItemGrab.cs
@@ -5,25 +5,77 @@ using UnityEngine;
 public class ItemGrab : MonoBehaviour
 {
 
-    private Vector3 offset;
-    private float mouseZLocation;
+    //private Vector3 offset;
+    //private float mouseZLocation;
 
+    float throwForce = 600;
+    Vector3 objectPos;
+    float distance;
 
-    public void PlayerInteract()
+    public bool canHold = true;
+    public GameObject item;
+    public GameObject tempParent;
+    public bool isHolding = false;
+
+    // Update is called once per frame
+    public void Update()
     {
-        print("grab");
-        mouseZLocation = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
-        offset = gameObject.transform.position - GetMouseWorldPos();
 
-        transform.position = GetMouseWorldPos() + offset;
+        distance = Vector3.Distance(item.transform.position, tempParent.transform.position);
+        //Check if isholding
+        if (isHolding == true)
+        {
+            item.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            item.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            item.transform.SetParent(tempParent.transform);
+            item.GetComponent<Rigidbody>().detectCollisions = true;
+            if (Input.GetMouseButtonDown(1))
+            {
+                
+                item.GetComponent<Rigidbody>().AddForce(tempParent.transform.forward * throwForce);
+                print(tempParent.transform.forward * throwForce);
+                isHolding = false;
+
+            }
+            if (Input.GetMouseButtonUp(0)) isHolding = false;
+        }
+        else
+        {
+            objectPos = item.transform.position;
+            item.transform.SetParent(null);
+            item.GetComponent<Rigidbody>().useGravity = true;
+            item.GetComponent<Rigidbody>().isKinematic = false;
+            item.transform.position = objectPos;
+        }
 
     }
-    private Vector3 GetMouseWorldPos()
+
+    public void OnMouseDown()
     {
+
+    }
+    public void OnMouseUp()
+    {
+        isHolding = false;
+    }
+public void PlayerInteract()
+    {
+        isHolding = true;
+        item.GetComponent<Rigidbody>().useGravity = false;
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        item.GetComponent<Rigidbody>().detectCollisions = true;
+        //mouseZLocation = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        //offset = gameObject.transform.position - GetMouseWorldPos();
+
+        //transform.position = GetMouseWorldPos() + offset;
+
+    }
+    //private Vector3 GetMouseWorldPos()
+    //{
         //get location of mouse
-        Vector3 mousePoint = Input.mousePosition;
-        mousePoint.z = mouseZLocation;
-        return Camera.main.ScreenToWorldPoint(mousePoint);
-    }
+        //Vector3 mousePoint = Input.mousePosition;
+        //mousePoint.z = mouseZLocation;
+        //return Camera.main.ScreenToWorldPoint(mousePoint);
+    //}
 
 }
