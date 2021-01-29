6bae9970c27d924686a4455936c7bd116062a6d5
Emily <fostere7@ferris.edu>
2021-01-29 18:45:43 -0500

    Working On the weight system

---
+++ Assets/Scripts/WeightedBoxes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightedBoxes : MonoBehaviour
+{
+
+    public float boxWeight = 0;
+
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
