faf9a1ac0de41b2604682cc2f8827dc6f9ad0526
PiggehCakes Jordan <47580405+PiggehCakes@users.noreply.github.com>
2021-01-30 23:36:28 -0500

    sceneschanges

---
+++ Assets/Scripts/SortingControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortingControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int numInDonate;
+    public static int NumInKeep;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (numInDonate == 5) print("5 in");
+    }
+    //private void OnTriggerEnter(Collider other)
+    ///{
+    //if (other.tag == "toy") 
+}
